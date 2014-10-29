855e4989c5aa064509525146b067eed1ff663bea
Nathan Brown <nkabrown@gmail.com>
2014-10-29 12:37:46 -0400

    Add List CRUD new
    -add lists#new route and view
    -create and migrate List constant

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,8 @@ class ListsController < ApplicationController
   def index
 
   end
+
+  def new
+    @list = List.new
+  end
 end
--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe ListsController, :type => :controller do
-  
+
+  let(:valid_attributes) {
+    @valid_attributes = {
+      title: "Groceries"
+    }
+  }
+
   describe "GET index" do
     it "should respond successfully with an HTTP 200 code" do
       get :index
 
       expect(response).to be_success
       expect(response).to have_http_status(200)
+    end
+
+    it "should render the index template" do
+      get :index
+
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET new" do
+    it "assigns a new list as @list" do
+      get :new, {}, valid_attributes
+
+      expect(assigns(:list)).to be_a_new(List)
     end
   end
 end
